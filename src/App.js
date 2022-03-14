4d2aaa91d0921a1d38c6af8ef0e6ee3d85c312bf
Yan Batista <ybatistasouza@hotmail.com>
2022-03-14 18:12:03 -0300

    app css layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+
+/* Components */
+import Die from './components/Die.js'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="App--title">Tenzi</h1>
+      <p className="App--description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+      <div className="dieContainer">
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+        <Die/>
+      </div>
+
+      <button className="App--button">Roll</button>
     </div>
   );
 }
---
+++ src/components/Die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Die() {
+    return(
+        <div className="die">
+            <p>1</p>
+        </div>
+    )
+}
